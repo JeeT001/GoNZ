e39c38050100234af82c23ce5136f3ef94255e2a
Simranjit Singh Babbar <111028218+JeeT001@users.noreply.github.com>
2023-12-04 13:37:36 +1300

    fixing errors
    Fixed the error of Urls page

--- tour/urls.py
+++ tour/urls.py
@@ -4,7 +4,7 @@ from . import views
 from tour.views import TourListView
 
 urlpatterns = [
-    path('tours/', TourListView, name="tour_list"),
+    path('tours/', TourListView.as_view(), name="tour_list"),
     # path('tours/', tour_list, name="tourList"),
     # path('tours/<int:pk>/', tour_detail, name="tourDetails"),
     # path('agents/', agents_list, name="agentList"),
