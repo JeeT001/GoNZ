0532441eb9ab666e70ccc966c864c9e0ba3c9004
Simranjit Singh Babbar <111028218+JeeT001@users.noreply.github.com>
2023-12-04 14:25:56 +1300

    Creating pages
    Creating views, urls and setting up the templates

--- tour/urls.py
+++ tour/urls.py
@@ -3,13 +3,21 @@ from django.urls import path
 from . import views
 from tour.views import TourListView
 from tour.views import AgentListView
+from tour.views import CustomerListView
+from tour.views import TourReviewView
+from tour.views import BookingTourView
 
 
 urlpatterns = [
     path('tours/', TourListView.as_view(), name="tour_list"),
-    # path('tours/', tour_list, name="tourList"),
+    path('tours/review/', TourReviewView.as_view(), name="tour_review"),
+    path('tours/booking/', BookingTourView.as_view(), name="booking_tour"),
+
     # path('tours/<int:pk>/', tour_detail, name="tourDetails"),
     path('agents/', AgentListView.as_view(), name="agent_list"),
     # path('agents/<int:pk>/', agents_detail, name="agentDetail"),
+    path('customers/', CustomerListView.as_view(), name="customer_list"),
+
+
 
 ]
--- tour/views.py
+++ tour/views.py
@@ -13,6 +13,21 @@ class TourListView(generic.ListView):
     template_name = 'tours/tour_list.html'
 
 
+class TourReviewView(generic.ListView):
+    model = TourReview
+    template_name = 'tours/tour_review.html'
+
+
+class BookingTourView(generic.ListView):
+    model = BookingTour
+    template_name = 'tours/booking_tour.html'
+
+
 class AgentListView(generic.ListView):
     model = Agents
     template_name = 'tours/agent_list.html'
+
+
+class CustomerListView(generic.ListView):
+    model = Customer
+    template_name = 'tours/customer_list.html'
