06f79af9715ca76e31b5c478c63a7e3e90c0cdb3
Simranjit Singh Babbar <111028218+JeeT001@users.noreply.github.com>
2023-12-09 21:55:51 +1300

    Updated templates
    Updated the views and models for templates

--- tour/urls.py
+++ tour/urls.py
@@ -2,20 +2,32 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from tour.views import TourListView
-from tour.views import AgentListView
+# from tour.views import AgentListView
 from tour.views import CustomerListView
 from tour.views import TourReviewView
-from tour.views import BookingTourView
+from tour.views import BookingTourListView
 from tour.views import TourCategoryView
+from tour.views import AgentDetailView
+from tour.views import AgentListView
+from tour.views import BookingTourDetailView
 
 
 urlpatterns = [
     path('tours/', TourListView.as_view(), name="tour_list"),
     path('tours/review/', TourReviewView.as_view(), name="tour_review"),
-    path('tours/booking/', BookingTourView.as_view(), name="booking_tour"),
+    path('tours/booking/', BookingTourListView.as_view(), name="booking_tour"),
+    path('tours/booking/<int:pk>/',
+         BookingTourDetailView.as_view(), name='booking_detail'),
+
 
     # path('tours/<int:pk>/', tour_detail, name="tourDetails"),
-    path('agents/', AgentListView.as_view(), name="agent_list"),
+    path('agents/list/', AgentListView.as_view(), name="agent_list"),
+
+    path('agents/details/<int:pk>/',
+         AgentDetailView.as_view(), name='agent_detail'),
+
+
+
     # path('agents/<int:pk>/', agents_detail, name="agentDetail"),
     path('customers/', CustomerListView.as_view(), name="customer_list"),
     path('tours/category/', TourCategoryView.as_view(), name="tour_category"),
--- tour/views.py
+++ tour/views.py
@@ -3,6 +3,8 @@ from django.views import generic
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from typing import Optional
+from django.views.generic import DetailView
+from django.views.generic import ListView
 
 
 from .models import Agents, Tours, TourReview, Customer, BookingTour, TourCategory
@@ -18,14 +20,34 @@ class TourReviewView(generic.ListView):
     template_name = 'tours/tour_review.html'
 
 
-class BookingTourView(generic.ListView):
+class BookingTourListView(ListView):
     model = BookingTour
     template_name = 'tours/booking_tour.html'
+    context_object_name = 'bookings'
 
 
-class AgentListView(generic.ListView):
+class BookingTourDetailView(DetailView):
+    model = BookingTour
+    template_name = 'tours/booking_detail.html'
+    context_object_name = 'booking'
+
+
+# class AgentListView(generic.ListView):
+#     model = Agents
+#     template_name = 'tours/agent_list.html'
+
+class AgentListView(ListView):
     model = Agents
+    # Set the template name as per your project structure
     template_name = 'tours/agent_list.html'
+    context_object_name = 'agentss'
+
+
+class AgentDetailView(DetailView):
+    model = Agents
+    # Set the template name as per your project structure
+    template_name = 'tours/agent_detail.html'
+    context_object_name = 'agent'
 
 
 class CustomerListView(generic.ListView):
